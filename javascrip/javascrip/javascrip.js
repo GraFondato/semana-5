171f19f3298ca4129abfbd4f2fcd531fde116d6c
grafondato <gracielafondato@gmail.com>
2022-11-20 23:38:03 -0300

    semana 5

---
+++ javascrip/javascrip/javascrip.js
@@ -0,0 +1,12 @@
+//variables
+
+var pais = "españa";
+var continente = "europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+pais = "mexico";
+continente = "latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);  
